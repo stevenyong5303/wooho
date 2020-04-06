d7b3f4acfdfad58f2c478c4c4ba0965d782705ae
Steven <stevenyong5303@msn.com>
2020-04-06 20:25:25 +0800

    1.0.7
    1. Tidy up files for app building.

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@ var apps = new Framework7({
 			  id: 'com.wkv.game',
 			  name: 'WOOHO',
 			  theme: 'md',
-			  version: "1.0.6",
+			  version: "1.0.7",
 			  rtl: false,
 			  language: "en-US"
 		  });
