3b1f24e71ead4bcd42a31cd453e40379c0ca568c
Steven <stevenyong5303@msn.com>
2020-04-07 10:39:36 +0800

    1.0.9
    1. Admob ID added.

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@ var apps = new Framework7({
 			  id: 'com.wkv.game',
 			  name: 'WOOHO',
 			  theme: 'md',
-			  version: "1.0.8",
+			  version: "1.0.9",
 			  rtl: false,
 			  language: "en-US"
 		  });
