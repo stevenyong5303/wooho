4e268aa947c3174b10e270e04289e757f377609c
Steven <stevenyong5303@msn.com>
2020-04-07 11:17:28 +0800

    1.0.11
    1. Update on config.xml.

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@ var apps = new Framework7({
 			  id: 'com.wkv.game',
 			  name: 'WOOHO',
 			  theme: 'md',
-			  version: "1.0.10",
+			  version: "1.0.11",
 			  rtl: false,
 			  language: "en-US"
 		  });
